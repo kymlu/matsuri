ac1e18692f4cd3a09d78a6cd7620d09a41bf5c1a
Katie <kymlu.4@gmail.com>
2025-09-12 23:41:08 +0900

    refactor header

---
+++ src/components/EditorPageHeader.tsx
@@ -0,0 +1,69 @@
+import React, { useContext, useState } from "react";
+import { CONTEXT_NAMES, DB_NAME } from "../data/consts.ts";
+import CustomMenu, { MenuItem, MenuSeparator } from "./CustomMenu.tsx";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../contexts/UserContext.tsx";
+import { Dialog } from "@base-ui-components/react";
+
+export function EditorPageHeader() {
+  const {selectedFormation, selectedFestival, updateState} = useContext(UserContext);
+  const navigate = useNavigate()
+  const [showSiteInfo, setShowSiteInfo] = useState<boolean>(false);
+  
+  return (
+    <>
+      <CustomMenu
+        trigger={
+          <img alt="Matsuri logo" src="logo192.png" className='size-8 max-w-8 max-h-8'/>
+        }>
+        <>
+          <MenuItem label="ホームに戻る" onClick={() => {
+            updateState({
+              selectedFormation: null,
+              selectedSection: null,
+              selectedFestival: null,
+              selectedItems: [],
+            });
+            navigate("../");
+          }} />
+          <MenuSeparator/>
+          <MenuItem label="サイト情報" onClick={() => {setShowSiteInfo(true)}}/>
+        </>
+      </CustomMenu>
+      {
+      selectedFormation && selectedFestival &&
+      <h1 className='px-2 font-bold text-center'>
+        {selectedFormation?.name} ({selectedFormation.width} x {selectedFormation.length}) ・ {selectedFestival?.name}
+      </h1>
+      }
+      <CustomMenu trigger={
+        <img alt="Extra settings" src="icons/settings.svg" className='size-8 max-w-8 max-h-8'/>
+        }>
+        <>
+          <MenuItem label="Clear Cache" onClick={() => {
+            Object.values(CONTEXT_NAMES).forEach((context) => {
+              localStorage.removeItem(context);
+            })}} />
+          <MenuSeparator />
+          <MenuItem label="Clear DB and Cache" onClick={() => {
+            Object.values(CONTEXT_NAMES).forEach((context) => {
+              localStorage.removeItem(context);
+            });
+            indexedDB.deleteDatabase(DB_NAME);
+          }} />
+        </>
+      </CustomMenu>
+      <Dialog.Root open={showSiteInfo} modal dismissible onOpenChange={() => setShowSiteInfo(false)}>
+          <Dialog.Portal>
+            <Dialog.Backdrop className="fixed inset-0 bg-black opacity-20 transition-all duration-150 data-[ending-style]:opacity-0 data-[starting-style]:opacity-0 dark:opacity-70" />
+            <Dialog.Popup className="fixed top-1/2 left-1/2 -mt-8 w-96 max-w-[calc(100vw-3rem)] -translate-x-1/2 -translate-y-1/2 rounded-lg bg-gray-50 p-6 text-gray-900 outline outline-1 outline-gray-200 transition-all duration-150 data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0 dark:outline-gray-300">
+              <Dialog.Title className="-mt-1.5 mb-1 text-lg font-medium">サイト情報</Dialog.Title>
+              <Dialog.Description className="mb-6 text-base text-gray-600">
+                工事中
+              </Dialog.Description>
+            </Dialog.Popup>
+          </Dialog.Portal>
+        </Dialog.Root>
+    </>
+  )
+}
--- src/pages/FormationEditorPage.tsx
+++ src/pages/FormationEditorPage.tsx
@@ -9,20 +9,20 @@ import { ParticipantCategory } from '../models/ParticipantCategory.ts';
 import { CategoryContext } from '../contexts/CategoryContext.tsx';
 import { FormationSongSection } from '../models/FormationSection.ts';
 import { isNullOrUndefined } from '../components/helpers/GlobalHelper.ts';
-import { CONTEXT_NAMES, DB_NAME, DEFAULT_WIDTH } from '../data/consts.ts';
+import { DEFAULT_WIDTH } from '../data/consts.ts';
 import { FormationContext } from '../contexts/FormationContext.tsx';
 import { Prop } from '../models/Prop.ts';
 import { Participant } from '../models/Participant.ts';
-import CustomMenu, { MenuItem, MenuSeparator } from '../components/CustomMenu.tsx';
-import { Dialog, Menu } from '@base-ui-components/react';
+import { Dialog } from '@base-ui-components/react';
 import { ExportContext } from '../contexts/ExportContext.tsx';
 import { PositionContext } from '../contexts/PositionContext.tsx';
 import { ParticipantPosition, PropPosition, NotePosition } from '../models/Position.ts';
+import { EditorPageHeader } from '../components/EditorPageHeader.tsx';
 
 export default function FormationEditorPage () {
   const userContext = useContext(UserContext);
   const {updateFormationContext} = useContext(FormationContext);
-  const {selectedFormation, selectedFestival, updateState} = useContext(UserContext);
+  const {selectedFormation, updateState} = useContext(UserContext);
   const {participantPositions, updatePositionState} = useContext(PositionContext);
   const {updateCategoryContext} = useContext(CategoryContext);
   const {isExporting, exportProgress} = useContext(ExportContext);
@@ -30,7 +30,6 @@ export default function FormationEditorPage () {
   const navigate = useNavigate()
   const formationEditorRef = React.createRef<any>();
   const [exportName, setExportName] = useState<string>("");
-  const [showSiteInfo, setShowSiteInfo] = useState<boolean>(false);
 
   useEffect(() => {
     Promise.all(
@@ -113,49 +112,10 @@ export default function FormationEditorPage () {
 
   return (
       <div className='h-full overflow-hidden'>
-        <div className='h-full min-h-0 overflow-hidden grid grid-cols-1 grid-rows-[10svh,90svh]'>
+        <div className='h-full min-h-0 overflow-hidden grid grid-cols-1 grid-rows-[60px_calc(100svh-60px)]'>
           <header className='flex items-center justify-between w-full col-span-3 px-4 py-2 border-b-2 border-solid border-grey'>
-            <CustomMenu trigger={
-              <img alt="Matsuri logo" src="logo192.png" className='size-8 max-w-8 max-h-8'/>
-            }>
-              <>
-                <MenuItem label="ホームに戻る" onClick={() => {
-                  updateState({
-                    selectedFormation: null,
-                    selectedSection: null,
-                    selectedFestival: null,
-                    selectedItems: [],
-                  });
-                  navigate("../");
-                }} />
-                <MenuSeparator/>
-                <MenuItem label="サイト情報" onClick={() => {setShowSiteInfo(true)}}/>
-              </>
-            </CustomMenu>
-            {
-            selectedFormation && selectedFestival &&
-            <h1 className='px-2 font-bold text-center'>
-              {selectedFormation?.name} ({selectedFormation.width} x {selectedFormation.length}) ・ {selectedFestival?.name}
-            </h1>
-            }
-            <CustomMenu trigger={
-              <img alt="Extra settings" src="icons/settings.svg" className='size-8 max-w-8 max-h-8'/>
-              }>
-              <>
-                <MenuItem label="Clear Cache" onClick={() => {
-                  Object.values(CONTEXT_NAMES).forEach((context) => {
-                    localStorage.removeItem(context);
-                  })}} />
-                <MenuSeparator />
-                <MenuItem label="Clear DB and Cache" onClick={() => {
-                  Object.values(CONTEXT_NAMES).forEach((context) => {
-                    localStorage.removeItem(context);
-                  });
-                  indexedDB.deleteDatabase(DB_NAME);
-                }} />
-              </>
-            </CustomMenu>
-            </header>
+            <EditorPageHeader/>
+          </header>
           <div className='flex flex-row gap-0'>
             <FormationLeftPanel/>
             <div className='flex flex-1 h-full min-h-0 overflow-auto'>
@@ -164,8 +124,6 @@ export default function FormationEditorPage () {
                 width={selectedFormation?.width ?? 20}
                 height={selectedFormation?.length ?? 20}/>
             </div>
-            {/* todo: warnings if some people aren't in all sections */}
-            {/* todo: warning if some people are in other categories */}
             <FormationRightPanel exportFunc={(exportName: string) => {
               formationEditorRef.current?.exportToPdf(exportName);
               setExportName(exportName);
@@ -179,17 +137,6 @@ export default function FormationEditorPage () {
               <Dialog.Title className="-mt-1.5 mb-1 text-lg font-medium">PDF出力中</Dialog.Title>
               <Dialog.Description className="mb-6 text-base text-gray-600">
                 <b>{exportName}.pdf</b>」を生成しています。<br></br>完了までしばらくお待ちください。<br></br>進行状況：{exportProgress}%
-              </Dialog.Description>
-            </Dialog.Popup>
-          </Dialog.Portal>
-        </Dialog.Root>
-        <Dialog.Root open={showSiteInfo} modal dismissible onOpenChange={() => setShowSiteInfo(false)}>
-          <Dialog.Portal>
-            <Dialog.Backdrop className="fixed inset-0 bg-black opacity-20 transition-all duration-150 data-[ending-style]:opacity-0 data-[starting-style]:opacity-0 dark:opacity-70" />
-            <Dialog.Popup className="fixed top-1/2 left-1/2 -mt-8 w-96 max-w-[calc(100vw-3rem)] -translate-x-1/2 -translate-y-1/2 rounded-lg bg-gray-50 p-6 text-gray-900 outline outline-1 outline-gray-200 transition-all duration-150 data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0 dark:outline-gray-300">
-              <Dialog.Title className="-mt-1.5 mb-1 text-lg font-medium">サイト情報</Dialog.Title>
-              <Dialog.Description className="mb-6 text-base text-gray-600">
-                工事中
               </Dialog.Description>
             </Dialog.Popup>
           </Dialog.Portal>
