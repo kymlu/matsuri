5d4b027331e40ad2ad9eb2441001f88d133d2a6e
Katie <kymlu.4@gmail.com>
2025-09-15 23:39:55 +0900

    Change browserrouter to hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import FormationEditorPage from './pages/FormationEditorPage.tsx';
 import FestivalManager from './pages/FestivalManager.tsx';
 import { RequireDB } from './data/RequireDb.tsx';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      {/* Routes */}
+    <HashRouter>
       <Routes>
         <Route path="/" element={<FestivalManager />} />
         <Route path="/formation" element={<RequireDB><FormationEditorPage /></RequireDB>} />
+        <Route path="*" element={<Navigate to="/" replace/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
