27339ca2209484863b48fc80b8937faa3dbc4209
Katie <kymlu.4@gmail.com>
2025-10-03 01:03:43 +0900

    add dynamic festival list

--- src/lib/helpers/DateHelper.ts
+++ src/lib/helpers/DateHelper.ts
@@ -10,3 +10,38 @@ export function formatExportDate(date = new Date()) {
 
   return `${year}${month}${day}${hours}${minutes}${seconds}`;
 }
+
+export function formatJapaneseDateRange(startDateString: string, endDateString?: string): string {
+  const startDate = new Date(startDateString);
+  if (isNaN(startDate.getTime())) {
+    throw new Error('Invalid date string');
+  }
+  
+  const year = startDate.getFullYear();
+  const month = startDate.getMonth() + 1; // getMonth() is 0-indexed
+  const day = startDate.getDate();
+  
+  var formattedString = `${year}年${month}月${day}日`;
+
+  if (endDateString) {
+    const endDate = new Date(endDateString);
+    if (isNaN(endDate.getTime())) {
+      throw new Error('Invalid date string');
+    }
+    if (endDate.getTime() !== startDate.getTime()) {
+      const endYear = endDate.getFullYear();
+      const endMonth = endDate.getMonth() + 1;
+      const endDay = endDate.getDate();
+      
+      if (endYear !== year) {
+        formattedString += `〜${endYear}年${endMonth}月${endDay}日`;
+      } else if (endMonth !== month) {
+        formattedString += `〜${endMonth}月${endDay}日`;
+      } else {
+        formattedString += `〜${endDay}日`;
+      }
+    }
+  }
+
+  return formattedString;
+}
--- src/pages/FestivalManager.tsx
+++ src/pages/FestivalManager.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import '../index.css';
 import Button from '../components/Button.tsx';
@@ -13,6 +13,8 @@ import { DEFAULT_GRID_SIZE, LAST_UPDATED } from '../lib/consts.ts';
 import { AppMode, AppModeContext } from '../contexts/AppModeContext.tsx';
 import { FormationContext } from '../contexts/FormationContext.tsx';
 import { VisualSettingsContext } from '../contexts/VisualSettingsContext.tsx';
+import allFestivals from "../data/allFestivals.json"
+import { formatJapaneseDateRange } from '../lib/helpers/DateHelper.ts';
 
 export default function FestivalManager () {
   const {updateState} = useContext(UserContext);
@@ -28,50 +30,89 @@ export default function FestivalManager () {
         showNotes: true,
     });
     updateFormationContext({selectedFormation: selectedFormation});    
-    updateVisualSettingsContext({gridSize: DEFAULT_GRID_SIZE})
+    updateVisualSettingsContext({gridSize: DEFAULT_GRID_SIZE});
     
     navigate("/formation");
   }
 
   return (
-    <div className='flex flex-col w-full gap-2'>
-      <div className='m-auto max-w-[90svw] mt-10'>
-        <div className='flex items-center justify-between'>
-          <h1 className='text-2xl font-bold'>祭り</h1>
-          <CustomToggleGroup
-            defaultValue="view"
-            label="モード"
-            options={[{
-              label:  "閲", // todo: change to icons
-              value: "view"
-            }, {
-              label: "編",
-              value: "edit"
-            }]}
-            onChange={(newValue) => updateAppModeContext({appMode: newValue as AppMode})}
-            currentValue={appMode}/>
-        </div>
-        <Divider primary/>
-        <div className='grid landscape:grid-cols-[auto,1fr] items-center gap-3'>
-          {
-            festivalList.map(festival =>
-              <React.Fragment key={festival.name}>
-                <h2 className='text-xl'>{festival.name}</h2>
-                <div className='flex flex-row flex-wrap gap-3'>
+    <div className='flex flex-col w-full gap-2 mt-10 portrait:my-10 landscape:max-w-[50svw] landscape:mx-auto'>
+      <div className='flex items-center justify-between mx-4'>
+        <h1 className='text-2xl font-bold'>祭り</h1>
+        {
+          appMode === "view" &&
+          <button
+            className='p-2 text-sm rounded-md text-grey-400 hover:bg-grey-100'
+            onClick={() => updateAppModeContext({appMode: "edit"})}>
+            振り班はこちらへ
+          </button>
+        }
+        {
+          appMode === "edit" &&
+          <button
+            className='p-2 text-sm rounded-md text-grey-400 hover:bg-grey-100'
+            onClick={() => updateAppModeContext({appMode: "view"})}>
+            一般の方はこちらへ
+          </button>
+        }
+      </div>
+      <Divider primary/>
+      {/* <div className='grid landscape:grid-cols-[auto,1fr] items-center gap-3 mx-4'>
+        {
+          festivalList.map(festival =>
+            <React.Fragment key={festival.name}>
+              <h2 className='text-xl'>{festival.name}</h2>
+              <div className='flex flex-row flex-wrap gap-3'>
+                {festival.formations
+                  .map(formation => 
+                    <Button 
+                      label={`Select ${formation.name}`}
+                      key={formation.id}
+                      onClick={(event) => {onClick(festival, formation)}}>
+                        {formation.name}
+                    </Button>
+                )}
+              </div>
+            </React.Fragment>
+          )
+        }
+      </div> */}
+      <div className='mx-4'>
+        {
+          allFestivals.festivals.sort((a, b) => new Date(b.startDate).getTime() - new Date(a.startDate).getTime())
+            .map((festival, index) =>
+            <div key={festival.id} className='mt-5'>
+              <div className='flex flex-row items-end gap-2'>
+                <h2 className='text-xl font-bold text-primary'>{festival.name}</h2>
+                <span className='text-sm text-grey-400'>{formatJapaneseDateRange(festival.startDate, festival.endDate)}</span>
+              </div>
+              <div>{festival.note}</div>
+              {
+                festival.formations && festival.formations.length > 0 && 
+                <div className='flex flex-row flex-wrap gap-3 mt-2'>
                   {festival.formations
                     .map(formation => 
                       <Button 
-                        label={`Select ${formation.name}`}
-                        key={formation.id}
-                        onClick={(event) => {onClick(festival, formation)}}>
-                          {formation.name}
+                        label={`Select ${formation}`}
+                        key={formation}
+                        onClick={() => {onClick({
+                          ...festival,
+                          startDate: new Date(festival.startDate),
+                          endDate: festival.endDate ? new Date(festival.endDate) : undefined,
+                          formations: [],
+                          } as Festival, {name: formation} as Formation)}}>
+                          {formation}
                       </Button>
                   )}
                 </div>
-              </React.Fragment>
-            )
-          }
-        </div>
+              }
+              {
+                index < allFestivals.festivals.length - 1 &&
+                <Divider />
+              }
+            </div>
+          )
+        }
       </div>
       <span className='fixed opacity-50 bottom-2 left-2'>{LAST_UPDATED}</span>
     </div>
